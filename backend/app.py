321f177e63c2bac7022a58694465c0b559026e14
SyaidiSuii <afiffhan@gmail.com>
2025-03-02 17:19:49 +0800

    menambahkan build

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pytesseract
 from PIL import Image
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
