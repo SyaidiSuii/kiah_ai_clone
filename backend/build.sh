4ce862546ca5391152221d6b40ed957717202548
SyaidiSuii <afiffhan@gmail.com>
2025-03-03 12:37:24 +0800

    ediit lagi

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 apt-get update && apt-get install -y tesseract-ocr
-pip install -r requirements.txt
-gunicorn -w 4 -b 0.0.0.0:10000 app:app
+pip install --no-cache-dir -r requirements.txt
